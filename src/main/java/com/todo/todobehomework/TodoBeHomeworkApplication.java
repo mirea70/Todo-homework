762c04de7d76aedb1049fdd44fe67f72bf3ea529
mirea70 <deae670@daum.net>
2022-08-22 16:52:00 +0900

    initial

---
+++ src/main/java/com/todo/todobehomework/TodoBeHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.todo.todobehomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoBeHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoBeHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/todo/todobehomework/TodoBeHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.todobehomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBeHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
